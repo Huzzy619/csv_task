d42f43cab40c311327abcdb4d8598a1e0bd16eda
Huzzy619 <blazingkrane@gmail.com>
2022-11-04 21:19:05 +0100

    update chip format

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import json
 import csv
 import os
-from hng import Helper 
+from hng import Helper
+
 
 def start():
 
     print("Hello and welcome")
 
-    filepath = input("Enter the file path or filename(if file is in current dir): ")
-        
+    filepath = input(
+        "Enter the file path or filename(if file is in current dir): ")
+
     process_file(filepath)
 
     print("""
@@ -29,12 +31,10 @@ def process_file(filepath):
     data = []
     help_to = Helper()
 
-
     try:
         # The sum of the entire entries minus the header
         total_entries = sum(1 for _ in open(filepath)) - 1
 
-    
         # Initial Opening of the csv file
         with open(filepath, encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file)
@@ -43,11 +43,8 @@ def process_file(filepath):
 
                 # Convert keys to lowercase
                 row = {key.lower(): val for key, val in row.items()}
-                value = row['filename']+'.json'
-                row['hash'] = help_to.convert_hash(value)
-                row['format'] = "CHIP-0007"
-                row['series_total'] = total_entries
-                row['sensitive_content'] = False
+
+                row['hash'] = help_to.convert_hash(row['filename']+'.json')
 
                 temp = row['attributes']
                 row['attributes'] = help_to.convert_to_dict(row['attributes'])
@@ -58,15 +55,34 @@ def process_file(filepath):
                 if not os.path.isdir('JSON'):
                     os.mkdir('JSON')
 
+                chip_json_format = {
+                    "format": "CHIP-007",
+                    "name": row["name"],
+                    "minting_tool": row["team names"],
+                    "sensitive_content": False,
+                    "series_number": row['series number'],
+                    "series_total": total_entries,
+                    "uuid": row['uuid'],
+                    "attributes": row['attributes'],
+                    "collection": {
+                        "name": "Zuri NFT Tickets for Free Lunch",
+                        "id": "b774f676-c1d5-422e-beed-00ef5510c64d",
+                        "attributes": [
+                            {
+                                "type": "description",
+                                "value": "Rewards for accomplishments duringHNGi9."
+                            }
+                        ]
+                    }
+                }
+
+
                 # converts each row entry to a json
                 with open(os.path.join('JSON', row['filename']+'.json'), 'w', encoding='utf-8') as jsonfile:
-                    jsonfile.write(json.dumps(row, indent=4))
+                    jsonfile.write(json.dumps(chip_json_format, indent=4))
 
-                # Remove the unnecessary values added before sending it to the new output csv.
+                # Restore the initial attributes.
                 row['attributes'] = temp
-                del row['format']
-                del row['series_total']
-                del row['sensitive_content']
 
                 data.append(row)
 
